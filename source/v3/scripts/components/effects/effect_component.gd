4b32244359fe33f09449101abb6c397fe6f756f7
Plagiatus <lukas.scheuerle@web.de>
2026-08-13 23:47:42 +0200

    whoops wrong effect component

--- source/v3/scripts/components/effects/effect_component.gd
+++ source/v3/scripts/components/effects/effect_component.gd
@@ -1,4 +1,5 @@
-class_name EffectComponent extends Node
+@abstract
+class_name EffectComponent extends Node2D
 
 @export var area_of_effect: Area2D
 
@@ -6,12 +7,23 @@ signal coin_entered_area(coin: Coin)
 signal coin_exited_area(coin: Coin)
 
 func _ready() -> void:
-	area_of_effect.body_entered.connect(coin_entered)
+	area_of_effect.body_entered.connect(_coin_entered)
+	area_of_effect.body_exited.connect(_coin_exited)
 
-func coin_entered(body: PhysicsBody2D) -> void:
+func _coin_entered(body: PhysicsBody2D) -> void:
 	if not body is Coin: return 
 	coin_entered_area.emit(body)
+	body.throw.connect(_on_coin_throw.bind(body))
+	body.land.connect(_on_coin_landed.bind(body))
 
-func coin_exited(body: PhysicsBody2D) -> void:
+func _coin_exited(body: PhysicsBody2D) -> void:
 	if not body is Coin: return 
 	coin_exited_area.emit(body)
+	body.throw.disconnect(_on_coin_throw)
+	body.land.disconnect(_on_coin_landed)
+
+func _on_coin_throw(_toss: TossResource, _coin: Coin): pass
+func _on_coin_landed(_toss_result: TossResultResource, _coin: Coin): pass
+
+func _draw() -> void:
+	draw_circle(position, 80, Color(1.0, 1.0, 1.0, 0.498), false, 1)
